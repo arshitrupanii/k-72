7eadc1ee9c219de534b763546978700b5a61facb
Arshit rupani <125478383+arshitrupanii@users.noreply.github.com>
2026-01-22 21:00:10 +0530

    asset goes into public folder

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,11 +2,11 @@ const Navbar = () => {
   return (
     <div className="w-screen fixed top-0 z-2 flex justify-between">
       <a href="/">
-        <img className="p-2" src="src/assets/logo.svg" alt="" />
+        <img className="p-2" src="logo.svg" alt="" />
       </a>
 
       <div className="w-[15vw] h-12 bg-black pr-10 cursor-pointer flex justify-end">
-        <img src="src/assets/menu.svg" alt="" />
+        <img src="menu.svg" alt="" />
       </div>
     </div>
   );

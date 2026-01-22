f560f5c76d7e623e3d12b92ea14f09718ef077af
Arshit rupani <125478383+arshitrupanii@users.noreply.github.com>
2026-01-22 20:53:21 +0530

    video location change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,11 +2,11 @@ const Navbar = () => {
   return (
     <div className="w-screen fixed top-0 z-2 flex justify-between">
       <a href="/">
-        <img className="p-2" src="public/logo.svg" alt="" />
+        <img className="p-2" src="src/assets/logo.svg" alt="" />
       </a>
 
       <div className="w-[15vw] h-12 bg-black pr-10 cursor-pointer flex justify-end">
-        <img src="public/menu.svg" alt="" />
+        <img src="src/assets/menu.svg" alt="" />
       </div>
     </div>
   );
